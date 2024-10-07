3dee8cef4c968105ae3629b559983885ee1d8fb2
asimrazzaq46 <asimrazzaq46@hotmail.com>
2024-10-07 14:56:24 +0200

    ImplimentationOfOrderfilteringOnBaseOfPayments

--- BulkyWeb/Areas/Admin/Controllers/OrderController.cs
+++ BulkyWeb/Areas/Admin/Controllers/OrderController.cs
@@ -1,10 +1,16 @@
 ﻿using Bulky.DataAcess.Repositery.IRepositery;
 using Bulky.Models.Models;
+using Bulky.Models.ViewModels;
+using Bulky.Utility;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
 
 namespace BulkyWeb.Areas.Admin.Controllers;
 
+[Area("admin")]
+//[Authorize(Roles =SD.Role_Admin)]
 public class OrderController : Controller
 {
 	private readonly IUnitOfWork _unitOfWork;
@@ -20,14 +26,44 @@ public class OrderController : Controller
 	}
 
 
-	#region API CALLS 
+    public IActionResult Detail(int orderId)
+    {
+        OrderVM orderVm = new()
+        {
+            OrderHeader = _unitOfWork.OrderHeader.GetOne(u => u.Id == orderId, includeProperties: "User"),
+            OrderDetail = _unitOfWork.OrderDetail.GetAll(u => u.OrderHeaderId == orderId, includeProperties: "Product")
+        };
+        return View(orderVm);
+    }
 
-	[HttpGet]
-	public IActionResult GetAll()
+
+    #region API CALLS 
+
+    [HttpGet]
+	public IActionResult GetAll(string status)
 	{
-		List<OrderHeader> objOrderHeaders = _unitOfWork.OrderHeader.GetAll(includeProperties: "User").ToList();
+		IEnumerable<OrderHeader> objOrderHeaders = _unitOfWork.OrderHeader.GetAll(includeProperties: "User").ToList();
 
-		return Json(new { data = objOrderHeaders });
+        switch (status)
+        {
+            case "pending":
+                objOrderHeaders = objOrderHeaders.Where(u=>u.PaymentStatus == SD.Payment_Status_DelayedPayment);
+                break;
+            case "inprocess":
+                objOrderHeaders = objOrderHeaders.Where(u => u.OrderStatus == SD.Status_Processing);
+                break;
+            case "completed":
+                objOrderHeaders = objOrderHeaders.Where(u => u.OrderStatus == SD.Status_Shipped);
+                break;
+            case "approved":
+                objOrderHeaders = objOrderHeaders.Where(u => u.OrderStatus == SD.Status_Approved);
+                break;
+            default:
+                break;
+
+        }
+
+        return Json(new { data = objOrderHeaders });
 	}
 
 
